c0c98fb468e956b9353742a9bc9eb353b0681dd2
Nwaburu Emeka Christian <exellentemy@gmail.com>
2026-01-26 06:22:49 +0100

    Create a Mint Account

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,18 +1,30 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
 import { createMint } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import wallet from "/home/christone/.config/solana/id.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
+console.log("Public Key is: " + keypair.publicKey);
+
+
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
+const connection = new Connection("http://localhost:8899", commitment);
 
 (async () => {
     try {
         // Start here
-        // const mint = ???
+        const mint = await createMint(
+            connection,
+            keypair,
+            keypair.publicKey,
+            null,
+            6
+        );
+
+        console.log("New Mint Account => " + mint);
+
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
